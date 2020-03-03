15130b740c1f6f446138463df70e8fa750c77a89
comxprt <johnnoah.ompad@yahoo.com>
2020-03-03 20:04:26 +0800

    Initial commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\API;
+use Illuminate\Http\Request; 
+use App\Http\Controllers\Controller; 
+use App\User; 
+use Illuminate\Support\Facades\Auth; 
+use Validator;
+
+class UserController extends Controller
+{
+	public $successStatus = 200;
+
+	/** 
+	     * Register api 
+	     * 
+	     * @return \Illuminate\Http\Response 
+	     */ 
+	    public function register(Request $request) 
+	    { 
+		        $validator = Validator::make($request->all(), [ 
+		            'name' => 'required', 
+		            'email' => 'required|email', 
+		            'password' => 'required', 
+		            'c_password' => 'required|same:password', 
+		        ]);
+		if ($validator->fails()) { 
+		            return response()->json(['error'=>$validator->errors()], 401);            
+		        }
+		$input = $request->all(); 
+		        $input['password'] = bcrypt($input['password']); 
+		        $user = User::create($input); 
+		        $success['token'] =  $user->createToken('MyApp')-> accessToken; 
+		        $success['name'] =  $user->name;
+		return response()->json(['success'=>$success], $this-> successStatus); 
+		    }
+
+
+}
---
+++ routes/api.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Http\Request;
+Use App\Article;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+
+Route::post('list','Users@list');
+
+Route::post('login', 'API\UserController@login');
+Route::post('register', 'API\UserController@register');
+
+Route::group(['middleware' => 'auth:api'], function(){
+Route::post('details', 'API\UserController@details');
+});
